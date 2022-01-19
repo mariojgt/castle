d6143eb23971b71d4a0881542340bed06eb40af2
AdminUI Action <noreply@dsmdesign.co.uk>
2022-01-19 18:53:47 +0000

    Github Action compiled resources

--- src/Helpers/SystemInfo.php
+++ src/Helpers/SystemInfo.php
@@ -1,14 +1,21 @@
 <?php
 namespace Mariojgt\Castle\Helpers;
+
 /**
 * System version info
 * [Description SystemInfo]
 */
+
 class SystemInfo
 {
     public function systemVersion()
     {
-        return 'v0.0.7-beta.2';
+        return 'v0.0.7-beta.3';
+    }
+
+    public function systemUpdates()
+    {
+        return '';
     }
 }
 
