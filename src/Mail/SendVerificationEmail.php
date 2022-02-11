2e9d458e53f7624caadd27bebc3470ea38adfbef
mario jose goes tarosso <mariojgt2@gmail.com>
2022-02-11 15:29:14 +0000

    up

--- src/Mail/SendVerificationEmail.php
+++ src/Mail/SendVerificationEmail.php
@@ -6,7 +6,6 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Carbon;
 
 class SendVerificationEmail extends Mailable implements ShouldQueue
 {
