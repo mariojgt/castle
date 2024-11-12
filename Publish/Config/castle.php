c04cbab8325dffb580ab28d05b6304b95d293374
Mario Tarosso <mariojgt2@gmail.com>
2024-11-12 21:39:06 +0000

    laravel-projects/packages/castle automatic push

--- Publish/Config/castle.php
+++ Publish/Config/castle.php
@@ -4,15 +4,15 @@ return [
     // If disable users Can't register in the website
     // IF IN PRODUCTION CHANGE THIS TO FALSE
     'demo_enable' => true,
-    // If disable users Can't register in the website
-    'sucess_login_route' => 'castle.try',
+
+    // You can define multiple guards, if you want to use multiple guards
+    'sucess_login_route' => [
+        'web'            => 'user.home', // Route to redirect after login
+        'skeleton_admin' => 'skeleton-admin.home', // Example
+    ],
     // If true means that the session can expire and will ask the authentication again
     'castle_session_can_expire' => true,
-    // Castle wall middleware session time, means how long the session can be active
+    // Castle wall middlewhere session time, means how long the session can be active
     'castle_wall_session_time'  => 60,
-    // Castle popup theme
-    // Supported themes: 'light','dark','cupcake','bumblebee','emerald','corporate','synthwave','retro','cyberpunk','valentine','halloween','garden','forest','aqua','lofi','pastel','fantasy','wireframe','black','luxury','dracula','cmyk','autumn','business','acid','lemonade','night','coffee','winter'
-    'castle_theme'  => 'coffee',
+    'castle_svg_size'          => 250,
 ];
-
-
