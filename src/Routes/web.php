f909ef3222bf0f0977d4e569f05626e4a328de89
Mario Tarosso <mariojgt2@gmail.com>
2025-01-08 15:04:38 +0000

    Update web.php

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -9,10 +9,10 @@ Route::group([
 ], function () {
     // middleware validation Render the authentication page
     Route::post('/castle/validate/2-steps/verification', [WallAuthenticationController::class, 'tryAuthentication'])
-        ->name('castle.validate');
+        ->name('castle.validate')->middleware(['recaptcha']);
     // Unlock castle code
     Route::post('/castle/unlock/account/backup/code', [WallAuthenticationController::class, 'tryUseBackupCode'])
-        ->name('castle.unlock.backup.code');
+        ->name('castle.unlock.backup.code')->middleware(['recaptcha']);
 
     // Render the view that display the castle wall
     Route::view('/castle/wall', 'castle::content.authentication.index')->name('castle.wall');
